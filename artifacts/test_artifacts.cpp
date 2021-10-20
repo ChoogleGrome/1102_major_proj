41dfee8824cd7e57dfc0a1ccaabb87a3617972dc
XTreved <lukeynewy@gmail.com>
2021-10-20 12:08:58 +1030

    Making sure everything is up

--- artifacts/test_artifacts.cpp
+++ artifacts/test_artifacts.cpp
@@ -6,24 +6,12 @@
 
 int main()
 {   
+    // Initialising player
     Player player;
 
     //Testing Player attribute and behaviours
     srand(time(0));
     
-    
-    // for (int j = 0; j < 100; j++)
-    // {
-    //     int randNum = (rand() % 10);
-    //     player.addItem(randNum);
-
-    //     std::cout << player.NumItems << std::endl;
-
-    //     for (int i = 0; i < 10; i++) {
-    //         std::cout << player.items[i].name << "\t\t" << player.items[i].amount << std::endl;
-    //     }
-    // }
-
     player.addItem(5);
     std::cout << player.critChance << std::endl;
 
